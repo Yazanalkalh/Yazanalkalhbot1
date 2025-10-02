1b519215ed1dca2cfd4a23f0646344ecbbc08a5c
Yazanalkalh <dood20301@gmail.com>
2025-10-02 12:13:39 +0300

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,22 +1,19 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class AdminStates(StatesGroup):
-    """
-    Defines the Finite State Machine (FSM) states for admin panel operations.
-    """
     waiting_for_new_reply = State()
+    waiting_for_delete_reply = State()
     waiting_for_new_reminder = State()
+    waiting_for_delete_reminder = State()
     waiting_for_new_channel_message = State()
+    waiting_for_delete_channel_msg = State()
+    waiting_for_instant_channel_post = State()
     waiting_for_ban_id = State()
     waiting_for_unban_id = State()
     waiting_for_broadcast_message = State()
     waiting_for_channel_id = State()
-    waiting_for_instant_channel_post = State()
     waiting_for_schedule_time = State()
     waiting_for_welcome_message = State()
     waiting_for_reply_message = State()
     waiting_for_media_reject_message = State()
-    waiting_for_delete_reply = State()
-    waiting_for_delete_reminder = State()
-    waiting_for_delete_channel_msg = State()
     waiting_for_clear_user_id = State()
