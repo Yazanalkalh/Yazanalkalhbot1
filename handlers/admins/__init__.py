c02b310e6341e795d663399e6289c86c50cb9f12
Yazanalkalh <dood20301@gmail.com>
2025-10-03 11:12:39 +0300

    Update __init__.py

--- handlers/admins/__init__.py
+++ handlers/admins/__init__.py
@@ -1,6 +1,8 @@
+from aiogram import Dispatcher
+
 from .panel import register_panel_handlers
 from .fsm_handlers import register_fsm_handlers
 
-def register_admin_handlers(dp):
+def register_admin_handlers(dp: Dispatcher):
     register_panel_handlers(dp)
     register_fsm_handlers(dp)
