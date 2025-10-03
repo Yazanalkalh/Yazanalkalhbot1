46855a32bea9f79ddd17e1073738078d4d08f401
Yazanalkalh <dood20301@gmail.com>
2025-10-03 03:52:00 +0300

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,19 +1,39 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class AdminStates(StatesGroup):
-    waiting_for_new_reply = State()
-    waiting_for_delete_reply = State()
+    # Dynamic Replies
+    waiting_for_dyn_reply_keyword = State()
+    waiting_for_dyn_reply_content = State()
+    waiting_for_dyn_reply_delete = State()
+    
+    # Reminders
     waiting_for_new_reminder = State()
     waiting_for_delete_reminder = State()
-    waiting_for_new_channel_message = State()
+    
+    # Channel Auto-Post Messages
+    waiting_for_new_channel_msg = State()
     waiting_for_delete_channel_msg = State()
     waiting_for_instant_channel_post = State()
+    
+    # Advanced Scheduling
+    waiting_for_scheduled_post_text = State()
+    waiting_for_scheduled_post_datetime = State()
+
+    # Ban Management
     waiting_for_ban_id = State()
     waiting_for_unban_id = State()
+    
+    # Broadcast
     waiting_for_broadcast_message = State()
-    waiting_for_channel_id = State()
-    waiting_for_schedule_time = State()
+    
+    # UI Customization
+    waiting_for_date_button_label = State()
+    waiting_for_time_button_label = State()
+    waiting_for_reminder_button_label = State()
+    waiting_for_timezone = State()
     waiting_for_welcome_message = State()
     waiting_for_reply_message = State()
     waiting_for_media_reject_message = State()
+    
+    # Memory Management
     waiting_for_clear_user_id = State()
