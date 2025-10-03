cdec4dcbabff363cd3077a181c737b252cfb1f83
Yazanalkalh <dood20301@gmail.com>
2025-10-03 09:14:36 +0300

    Create __init__.py

---
+++ handlers/users/__init__.py
@@ -0,0 +1,8 @@
+from .start import register_start_handlers
+from .callback_handler import register_callback_handlers
+from .message_handler import register_message_handlers
+
+def register_user_handlers(dp):
+    register_start_handlers(dp)
+    register_callback_handlers(dp)
+    register_message_handlers(dp)
