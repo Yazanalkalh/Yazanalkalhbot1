b36c6ee4d205e3e1623988a22ad2949d133d413b
Yazanalkalh <dood20301@gmail.com>
2025-10-03 11:11:41 +0300

    Update __init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,8 +1,10 @@
+from aiogram import Dispatcher
+
 from .start import register_start_handlers
 from .callback_handler import register_callback_handlers
 from .message_handler import register_message_handlers
 
-def register_user_handlers(dp):
+def register_user_handlers(dp: Dispatcher):
     register_start_handlers(dp)
     register_callback_handlers(dp)
     register_message_handlers(dp)
