e9ffde7cca7a8e57ee6075040d4037a1290c25ce
Yazanalkalh <dood20301@gmail.com>
2025-10-03 04:45:59 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,11 +2,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import BOT_TOKEN
 
-# تهيئة مخزن الحالات في الذاكرة
 storage = MemoryStorage()
-
-# إنشاء كائن البوت
-bot = Bot(token=BOT_TOKEN, parse_mode="Markdown")
-
-# إنشاء كائن المرسل (Dispatcher)
+bot = Bot(token=BOT_TOKEN, parse_mode="HTML") # Using HTML for better formatting
 dp = Dispatcher(bot, storage=storage)
