e7f5e591d96bda14126c7268d361ec948254849a
Yazanalkalh <dood20301@gmail.com>
2025-10-03 12:18:14 +0300

    Update __init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,9 +2,10 @@ from aiogram import Dispatcher
 
 from .start import register_start_handlers
 from .callback_handler import register_callback_handlers
-from .message_handler import register_message_handlers
+from .message_handler import register_message_handler # <-- Corrected from plural to singular
 
 def register_user_handlers(dp: Dispatcher):
+    """Registers all user-side handlers."""
     register_start_handlers(dp)
     register_callback_handlers(dp)
-    register_message_handlers(dp)
+    register_message_handler(dp) # <-- Corrected from plural to singular
