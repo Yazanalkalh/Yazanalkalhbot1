c14d6057d0213d9185911116fe192e3412423329
Yazanalkalh <dood20301@gmail.com>
2025-10-03 09:18:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
-import os
-import asyncio
-from flask import Flask
-from threading import Thread
 from aiogram.utils import executor
 from loader import dp
-from handlers import admin, user
+from handlers import register_all_handlers
 import data_store
-from utils.tasks import startup_tasks
+from utils.background_tasks import startup_tasks
+from web_server import start_web_server_thread
 
-# --- Web Server to Keep Bot Alive on Render ---
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is alive and running!"
-
-def run_web_server():
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
-
-# --- Bot Startup Logic ---
 async def on_startup(dispatcher):
-    """Function to run on bot startup."""
     data_store.initialize_data()
-    
-    # Register handlers
-    admin.register_admin_handlers(dispatcher)
-    user.register_user_handlers(dispatcher)
-    
-    # Run background tasks
+    register_all_handlers(dispatcher)
     await startup_tasks(dispatcher)
+    print("Bot is up and running!")
 
 if __name__ == '__main__':
-    # Start the web server in a separate thread
-    web_thread = Thread(target=run_web_server)
-    web_thread.daemon = True
-    web_thread.start()
-
-    # Start the bot
-    print("Starting bot polling...")
+    start_web_server_thread()
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
