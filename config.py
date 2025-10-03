229a26521aa22d43798a4b3b946fc8dc269dea6e
Yazanalkalh <dood20301@gmail.com>
2025-10-03 07:34:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
 MONGO_URI = os.getenv("MONGO_URI")
 
 if not all([API_TOKEN, ADMIN_CHAT_ID, MONGO_URI]):
-    print("❌ FATAL: Missing one or more environment variables (BOT_TOKEN, ADMIN_CHAT_ID, MONGO_URI)")
+    print("❌ FATAL ERROR: BOT_TOKEN, ADMIN_CHAT_ID, or MONGO_URI not found in environment variables!")
     exit(1)
