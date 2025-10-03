e24dddfa8dfc6d729ded40a79953deaad933a0b6
Yazanalkalh <dood20301@gmail.com>
2025-10-03 09:15:40 +0300

    Create __init__.py

---
+++ handlers/admins/__init__.py
@@ -0,0 +1,6 @@
+from .panel import register_panel_handlers
+from .fsm_handlers import register_fsm_handlers
+
+def register_admin_handlers(dp):
+    register_panel_handlers(dp)
+    register_fsm_handlers(dp)
