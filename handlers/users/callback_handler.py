9ead58e71464d33079e1bb60461ac3088f2a0c17
Yazanalkalh <dood20301@gmail.com>
2025-10-03 19:52:01 +0300

    Update callback_handler.py

--- handlers/users/callback_handler.py
+++ handlers/users/callback_handler.py
@@ -2,10 +2,16 @@ from aiogram import types, Dispatcher
 from loader import bot
 import data_store
 from utils.helpers import get_hijri_date_str, get_live_time_str, get_random_reminder
-from .message_handler import is_regular_user
+
+# We don't need a special filter here, this should work for any non-banned user.
 
 async def callback_query_handler(cq: types.CallbackQuery):
     """Handler for user inline button presses."""
+    # Ban Check
+    if cq.from_user.id in data_store.bot_data['banned_users']:
+        await cq.answer("🚫 أنت محظور من استخدام البوت.", show_alert=True)
+        return
+
     await cq.answer()
     
     response_text = ""
@@ -15,18 +21,17 @@ async def callback_query_handler(cq: types.CallbackQuery):
         response_text = get_live_time_str()
     elif cq.data == "show_reminder":
         response_text = get_random_reminder()
-    else:
-        return
-
+    
     if response_text:
-        await bot.send_message(
-            cq.from_user.id, 
-            response_text,
-            protect_content=data_store.bot_data['bot_settings'].get('content_protection', False)
-        )
+        try:
+            await bot.send_message(
+                chat_id=cq.from_user.id, 
+                text=response_text,
+                protect_content=data_store.bot_data['bot_settings'].get('content_protection', False)
+            )
+        except Exception as e:
+            print(f"Error sending callback response: {e}")
 
-# --- THIS IS THE MISSING PIECE THAT FIXES THE ERROR ---
 def register_callback_handler(dp: Dispatcher):
     """Registers the callback query handler for regular users."""
-    # We add the is_regular_user filter to ensure admins don't accidentally trigger this
-    dp.register_callback_query_handler(callback_query_handler, is_regular_user)
+    dp.register_callback_query_handler(callback_query_handler, state=None)
