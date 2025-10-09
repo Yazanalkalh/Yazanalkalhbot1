4794795675e6f90d28f34adaf9ce6f8c1ce6c24b
Yazanalkalh <dood20301@gmail.com>
2025-10-09 06:10:57 +0300

    Update __init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,11 +1,11 @@
 from aiogram import Dispatcher
-
-from .start import register_start_handler
-from .callback_handler import register_callback_handler
-from .message_handler import register_message_handler
+from .start import register_start_handlers
+from .message_handler import register_user_message_handler
+from .callback_handler import register_user_callback_handler
 
 def register_user_handlers(dp: Dispatcher):
-    """Registers all handlers for regular users."""
-    register_start_handler(dp)
-    register_callback_handler(dp)
-    register_message_handler(dp)
+    # Handlers with specific commands/filters first
+    register_start_handlers(dp)
+    register_user_callback_handler(dp)
+    # The general message handler last, to catch everything else
+    register_user_message_handler(dp)
