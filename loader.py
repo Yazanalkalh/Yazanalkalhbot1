3f85de3453c46685db54bf79e6d148b373f668af
Yazanalkalh <dood20301@gmail.com>
2025-10-09 05:49:13 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,6 +2,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import API_TOKEN
 
+# Using MemoryStorage for FSM states. It's simple and reliable for most cases.
 storage = MemoryStorage()
+
+# Initialize the bot with HTML parsing mode.
 bot = Bot(token=API_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=storage)
