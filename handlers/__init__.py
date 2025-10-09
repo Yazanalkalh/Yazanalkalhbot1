2e3cf474373a1e336fc70753b48348cf49832454
Yazanalkalh <dood20301@gmail.com>
2025-10-09 06:09:10 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,9 @@
 from aiogram import Dispatcher
-
 from .admins import register_admin_handlers
 from .users import register_user_handlers
 
 def register_all_handlers(dp: Dispatcher):
-    # المهم: تسجيل معالجات المشرفين أولاً
+    # Admin handlers are registered first to have priority
     register_admin_handlers(dp)
-    
-    # ثم تسجيل معالجات المستخدمين
+    # User handlers are registered second
     register_user_handlers(dp)
