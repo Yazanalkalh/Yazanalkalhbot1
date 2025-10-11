f7c78387bb255e460fb11ffec1450c8fbdb3bba6
Yazanalkalh <dood20301@gmail.com>
2025-10-11 21:17:59 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-API_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
-MONGO_URI = os.getenv("MONGO_URI")
-
-if not all([API_TOKEN, ADMIN_CHAT_ID, MONGO_URI]):
-    print("❌ FATAL ERROR: BOT_TOKEN, ADMIN_CHAT_ID, or MONGO_URI not found in environment variables!")
-    exit(1)
